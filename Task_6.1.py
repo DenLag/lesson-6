d18f5926cb7615bf7a0f23af5a57431807ec2579
DenLag <denislagunov@yandex.ru>
2022-06-23 16:07:10 +0500

    add file

---
+++ Task_6.1.py
@@ -0,0 +1,20 @@
+from time import sleep
+
+
+class TrafficLight:
+    __color = "Чёрный"
+
+    def running(self):
+        while True:
+            print('TrafficLight is red')
+            sleep(7)
+            print('TrafficLight is yellow')
+            sleep(2)
+            print('TrafficLight is green')
+            sleep(7)
+            print('TrafficLight is yellow')
+            sleep(2)
+
+
+trafficlight = TrafficLight()
+trafficlight.running()
